31f9243713348afefe72e77110d7cb2daf6d8124
Darío Fernández <dariofedes@protonmail.com>
2021-01-29 18:43:07 +0100

    adding user schema

---
+++ schemas/user.js
@@ -0,0 +1,10 @@
+const { Schema, Types: { ObjectId } } = require('mongoose')
+
+module.exports = new Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    signUpDate: { type: Date, required: true, default: new Date },
+    publications: [{ type: ObjectId, ref: 'Publication' }],
+    liked: [{ type: ObjectId, ref: 'Publication' }],
+})
